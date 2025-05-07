1547babf605c21bb5a5577c5ae72de422a11b4d2
oscarcv86 <oscarcv86@hotmail.com>
2025-05-06 21:55:40 -0500

    primera version

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/Dani')
+def dani():
+    return render_template('dani.html', numeros=range(1, 101))
+
+@app.route('/Taty')
+def taty():
+    return render_template('taty.html')
+
+@app.route('/Oscar')
+def oscar():
+    return render_template('oscar.html')
+
+@app.route('/formulario', methods=['GET', 'POST'])
+def formulario():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido1 = request.form['apellido1']
+        apellido2 = request.form['apellido2']
+        saludo = f"¡Hola {nombre} {apellido1} {apellido2}!"
+        return render_template('formulario.html', saludo=saludo)
+    return render_template('formulario.html', saludo=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
